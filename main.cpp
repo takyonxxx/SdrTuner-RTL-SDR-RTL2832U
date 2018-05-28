cdef83edc13b0e957151d2b4e96133e65a41a5de
TÜRKAY BİLİYOR <turkaybiliyor@hotmail.com>
2018-05-28 11:27:19 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -5,20 +5,6 @@ int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
     SdrWindow w;
-    w.show();
-    int opt;
-    while ((opt = getopt(argc, argv, "d:f:g:s:b:l:o:t:r:p:E:F:A:M:h")) != -1)
-    {
-       switch (opt) {
-       case 'M':
-           if (strcmp("wbfm",  optarg) == 0)
-           {
-           }
-           break;
-       case 'h':
-       default:
-           break;
-       }
-    }
+    w.show();    
     return a.exec();
 }
